5d0560953a582067f9a5b084221a51f852a77f1b
lucianofer02 <lucianofernandez122@gmail.com>
2022-09-22 14:15:23 -0300

    Ej01

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -17,7 +17,14 @@ class Circle:
         - No utilizar Properties
         - Utilizar Type Hints en todos los métodos y variables
     """
+    def __init__(self, radio: float):
+        self.radio: float = radio 
 
+    def area(self):
+        return (self.radio**2) * 3.14 
+
+    def perimetro(self):
+        return 2 * 3.14 * self.radio
 
 # NO MODIFICAR - INICIO
 # Test básico
